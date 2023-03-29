a37f4cd537bf4f1a96bc8d3a83e992a8e0e58f44
stephen Batelaan <sbatelaan91@gmail.com>
2023-03-28 18:28:23 -0700

    final code, needs edit readme

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -27,6 +27,7 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({ message: 'No tag with this id'});
       return;
     }
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err)
   }
